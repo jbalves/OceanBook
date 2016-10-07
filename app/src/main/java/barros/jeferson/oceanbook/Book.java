0128c1aa83bc756040c833a4c539f27737c47bd6
Jeferson Barros <im.jbalves@gmail.com>
2016-10-07 00:53:07 -0300

    Setup book class

---
+++ app/src/main/java/barros/jeferson/oceanbook/Book.java
@@ -0,0 +1,53 @@
+package barros.jeferson.oceanbook;
+
+/**
+ * Created by aluno on 07/10/2016.
+ */
+
+public class Book {
+    private String capa;
+    private String titulo;
+    private String autor;
+    private Integer paginas;
+    private Integer ano;
+
+    public String getCapa() {
+        return capa;
+    }
+
+    public void setCapa(String capa) {
+        this.capa = capa;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public Integer getPaginas() {
+        return paginas;
+    }
+
+    public void setPaginas(Integer paginas) {
+        this.paginas = paginas;
+    }
+
+    public Integer getAno() {
+        return ano;
+    }
+
+    public void setAno(Integer ano) {
+        this.ano = ano;
+    }
+}
